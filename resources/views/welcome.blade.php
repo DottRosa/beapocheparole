f31369a0bde6a6a556c474919dfbafa1c262b933
Marco Rosa <marco1rosa@gmail.com>
2018-09-23 14:21:02 +0200

    Ultima prova di commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel funziona
+                    Laravel
                 </div>
 
                 <div class="links">
