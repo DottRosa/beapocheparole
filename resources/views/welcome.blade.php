1b6327b8fa1395761d4cea60d0aa77d2d83a7b82
Marco Rosa <marco1rosa@gmail.com>
2018-09-28 08:29:37 +0200

    Configurazione di Gulp e aggiornamento del Bignami

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,8 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
 
+        <link rel="stylesheet" type="text/css" href="{{ url('/css/app.css') }}" />
+
         <!-- Styles -->
         <style>
             html, body {
