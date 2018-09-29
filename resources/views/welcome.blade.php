82e824f33d2ba8baaab20db0a4f3932442894d6b
Marco Rosa <marco1rosa@gmail.com>
2018-09-29 10:51:07 +0200

    Bignami

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Beapocheparole
                 </div>
 
                 <div class="links">
