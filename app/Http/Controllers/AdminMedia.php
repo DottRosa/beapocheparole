54d46e973795d1ff1f328cf12c4c223d1397ff11
Eleonora Macuglia <elemac93@gmail.com>
2018-10-14 13:34:41 +0200

    aggiunta query media_tags

--- app/Http/Controllers/AdminMedia.php
+++ app/Http/Controllers/AdminMedia.php
@@ -16,21 +16,48 @@ class AdminMedia extends Controller{
         $offset = $request->offset;
         $limit = $request->limit;
         $type = $request->type;
+        $tags = array('1');
 
         //TODO cercare anche tra i nomi dei tag
-        $items = Media::where([
-                    ['title', 'like', '%'.$q.'%'],
-                    ['type', '=', $type]
-                 ])
-                  ->offset($offset)
-                  ->limit($limit)
-                  ->get();
-
-        $total = Media::where([
+        if(count($tags) == 0){
+          $itemsFromTitle = Media::where([
                       ['title', 'like', '%'.$q.'%'],
                       ['type', '=', $type]
-                 ])
-                 ->count();
+                   ])
+                    ->offset($offset)
+                    ->limit($limit)
+                    ->get();
+
+          $total = Media::where([
+                        ['title', 'like', '%'.$q.'%'],
+                        ['type', '=', $type]
+                   ])
+                   ->count();
+
+        }else{
+          $sqlMedia = 'SELECT *
+                      FROM R_MEDIA_TAGS INNER JOIN MEDIA ON R_MEDIA_TAGS.media_id=MEDIA.id
+                      WHERE ';
+          foreach ($tags as $tag) {
+            $sqlMedia .= 'R_MEDIA_TAGS.tag_id=\''.$tag.'\' AND ';
+          }
+          $sqlMedia .= 'MEDIA.type=\''$type'\' AND MEDIA.title LIKE \'%'$q'%\'';
+          $sqlMedia .= ' LIMIT '.$limit;
+          $sqlMedia .= ' OFFSET '.$offset;
+
+          $itemsFromTag = DB::select(DB::raw($sqlMedia));
+
+          $sqlMediaTotal = 'SELECT *
+                      FROM R_MEDIA_TAGS INNER JOIN MEDIA ON R_MEDIA_TAGS.media_id=MEDIA.id
+                      WHERE ';
+          foreach ($tags as $tag) {
+            $sqlMediaTotal .= 'R_MEDIA_TAGS.tag_id=\''.$tag.'\' AND ';
+          }
+          $sqlMediaTotal .= 'MEDIA.type=\''$type'\' AND MEDIA.title LIKE \'%'$q'%\'';
+
+          $total = DB::select(DB::raw($sqlMediaTotal))
+                   ->count();
+        }
 
         $result = array(
             'total' => $total,
