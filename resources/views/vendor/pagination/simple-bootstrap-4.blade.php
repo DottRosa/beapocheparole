a1c8bf29587ca1dcf5e6822606a7405d8b7ef086
Marco Rosa <marco1rosa@gmail.com>
2018-10-24 23:28:46 +0200

    Paginazione custom

---
+++ resources/views/vendor/pagination/simple-bootstrap-4.blade.php
@@ -0,0 +1,25 @@
+@if ($paginator->hasPages())
+    <ul class="pagination" role="navigation">
+        {{-- Previous Page Link --}}
+        @if ($paginator->onFirstPage())
+            <li class="page-item disabled" aria-disabled="true">
+                <span class="page-link">< Precedente</span>
+            </li>
+        @else
+            <li class="page-item">
+                <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev">< Precedente </a>
+            </li>
+        @endif
+
+        {{-- Next Page Link --}}
+        @if ($paginator->hasMorePages())
+            <li class="page-item">
+                <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next">Successivo ></a>
+            </li>
+        @else
+            <li class="page-item disabled" aria-disabled="true">
+                <span class="page-link">Successivo ></span>
+            </li>
+        @endif
+    </ul>
+@endif
